da705401daf4908356e3f7fdcd068d5940f8e828
Deepak <wadkarkalpesh96@gmail.com>
2026-02-23 22:54:54 +0530

    Day 6: 3Sum (Two Pointers)

---
+++ Arrays/three_sum.md
@@ -0,0 +1,14 @@
+# 3Sum
+
+Topic: Two Pointers + Sorting
+
+## Approach
+Sort the array.
+Fix one element and use two pointers to find remaining pair.
+Skip duplicate values to avoid repeated triplets.
+
+## Time Complexity
+O(n²)
+
+## Space Complexity
+O(1) (excluding output)
---
+++ Arrays/three_sum.py
@@ -0,0 +1,33 @@
+class Solution:
+    def threeSum(self, nums):
+        nums.sort()
+        result = []
+
+        for i in range(len(nums)):
+
+            # skip duplicate fixed element
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+
+            left = i + 1
+            right = len(nums) - 1
+
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
+
+                if total == 0:
+                    result.append([nums[i], nums[left], nums[right]])
+
+                    left += 1
+                    right -= 1
+
+                    # skip duplicates
+                    while left < right and nums[left] == nums[left-1]:
+                        left += 1
+
+                elif total < 0:
+                    left += 1
+                else:
+                    right -= 1
+
+        return result
