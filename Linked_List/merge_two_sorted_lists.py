9086353b55b84b72191d0e7bcbeb587efff68308
Deepak <wadkarkalpesh96@gmail.com>
2026-03-05 21:58:53 +0530

    Updated Merge Two Sorted Lists solution

--- Linked_List/merge_two_sorted_lists.py
+++ Linked_List/merge_two_sorted_lists.py
@@ -1,3 +1,8 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
 class Solution:
     def mergeTwoLists(self, list1, list2):
         dummy = ListNode(0)
